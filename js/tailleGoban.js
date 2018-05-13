906f8c1dc7e7affd268f435ca9724f216641ea52
clement.lebas <clement.lebas77@live.fr>
2018-05-13 14:04:18 +0200

    second commit

--- js/tailleGoban.js
+++ js/tailleGoban.js
@@ -2,12 +2,11 @@ console.log("Taille goban JS vient d'être chargé");
 
     var tailleGoban = $('#gobanC >tbody >tr').length;
     var taille;
+    var a;
+    var b;
+    var centrage;
 var tailleFunction = function (event) {
         
-        var a;
-        var b;
-        
-        console.log("click 9");
         console.log(tailleGoban);
         $('#gobanC >tbody').remove();
         $('#goban >tbody').remove();
@@ -27,16 +26,25 @@ var tailleFunction = function (event) {
             
         // DOUBLE BOUCLE APPEND HOSHI
         if(taille==9){
+            centrage = {width:'400px', 'margin-left': '-200px'
+                        };      
+            $("#gobanC").css(centrage);
         for(i=2;i<7;i=i+2)
             for(j=2;j<7;j=j+2)
                 $("#goban tr").eq(i).children().eq(j).append('<img class="hoshi" src="./image/hoshi.png" alt="">');     
         }
         else if(taille==13){
+            centrage = {width:'560px', 'margin-left': '-280px'
+                        };      
+            $("#gobanC").css(centrage);
             for (i = 3; i < 10; i = i + 3)
                 for (j = 3; j < 10; j = j + 3)
                     $("#goban tr").eq(i).children().eq(j).append('<img class="hoshi" src="./image/hoshi.png" alt="">');
         }
         else{
+            centrage = {width:'820px', 'margin-left': '-410px'
+                        };      
+            $("#gobanC").css(centrage);
             for (i = 3; i < 16; i = i + 6)
                 for (j = 3; j < 16; j = j + 6)
                     $("#goban tr").eq(i).children().eq(j).append('<img class="hoshi" src="./image/hoshi.png" alt="">'); 
@@ -45,5 +53,5 @@ var tailleFunction = function (event) {
         tailleGoban = $('#gobanC >tbody >tr').length;
         
     }
-S
+
 
